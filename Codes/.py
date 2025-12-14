360a4d39197aab25043d1a8242034cbaf04899ff
belugaman9981 <littledage@outlook.com>
2025-12-13 20:19:41 -0800

    Update .py

--- Codes/.py
+++ Codes/.py
@@ -2,13 +2,23 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-pic = pygame.image.load("rain.jpg")
+pic = pygame.image.load("notapng.png")
 i = True
+
+picx = 0
+picy = 0
+BLACK = (0, 0, 0)
+timer = pygame.time.Clock()
 
 while i:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             i = False
+            
+    picx += 1
+    picy += 1
+    
+    screen.fill(BLACK)
     screen.blit(pic, (0, 0))
     pygame.display.flip()
     
